8a06dda6a0d5d0c6e9493b8064a9e3f794465df5
M Iqbal Alamsyah S <ialamsyah92@gmail.com>
2024-04-29 18:59:09 +0700

    enhancement/visit-request

--- resources/js/Pages/FormVisit.vue
+++ resources/js/Pages/FormVisit.vue
@@ -13,22 +13,22 @@ import { reactive, ref } from "vue";
 const styleStore = useStyleStore();
 
 const form = useForm({
-    visit_purpose: "Survey",
-    start_date: new Date(),
-    end_date: new Date(),
-    description: "haloo",
+    visit_purpose: "",
+    start_date: "",
+    end_date: "",
+    description: "",
     spk: null,
     visitors: [],
 });
 
 const initialVisitor = {
-    ktp: "321391203123",
-    name: "ibangg",
+    ktp: "",
+    name: "",
     file_ktp: "",
-    company: "PT ibang",
-    occupation: "CEO",
-    phone: "08192381231",
-    email: "ias@asd.sad",
+    company: "",
+    occupation: "",
+    phone: "",
+    email: "",
     show: true,
 };
 
@@ -63,7 +63,7 @@ const submit = () => {
     <LayoutGuest>
         <Head title="Form" />
         <div
-            class="relative flex flex-col items-center selection:bg-blue-500 selection:text-white"
+            class="relative min-h-screen flex flex-col items-center selection:bg-blue-500 selection:text-white"
         >
             <NotificationBar
                 :key="Date.now()"
@@ -79,7 +79,7 @@ const submit = () => {
                 {{ $page.props.flash.message }}
             </NotificationBar>
             <div class="flex flex-col lg:flex-row w-full">
-                <div class="w-full h-screen lg:w-1/4 flex flex-col justify-between items-center fixed py-10 dark:bg-slate-900/70 bg-white">
+                <div class="w-full lg:h-screen lg:w-1/4 flex flex-col justify-between items-center lg:fixed pt-10 gap-5 lg:py-10 dark:bg-slate-900/70 bg-white">
                     <div class="flex items-center gap-5">
                         <div class="logo1">
                             <img src="../../img/direktorat-logo.png" class="max-w-[75px] lg:max-w-[100px] inline-block rounded-full ring-2 ring-white bg-white" />
@@ -91,13 +91,13 @@ const submit = () => {
                     <div>
                         <span class="text-3xl font-semibold">Visit Request App</span>
                     </div>
-                    <div>
+                    <div class="hidden lg:flex">
                         <img src="../../img/illustration.png" class="p-2" />
                     </div>
                 </div>
                 <form
                     @submit.prevent="submit"
-                    class="relative lg:absolute lg:right-0 w-full lg:w-3/4 flex flex-col p-8 dark:bg-slate-900/70 bg-white"
+                    class="relative lg:left-1/4 w-full lg:w-3/4 flex flex-col p-8 dark:bg-slate-900/70 bg-white"
                 >
                     <div class="flex flex-col gap-8 lg:flex-row">
                         <div class="flex flex-col w-full lg:w-50">
